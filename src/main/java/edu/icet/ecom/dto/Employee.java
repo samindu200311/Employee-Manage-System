7d9993a8edebe352db4a39fe3574d8a261ab3d50
samindu <ssamindudeneth04@gmail.com>
2025-04-29 16:22:50 +0530

    add-createdAt and updatedAt in Employee

--- src/main/java/edu/icet/ecom/dto/Employee.java
+++ src/main/java/edu/icet/ecom/dto/Employee.java
@@ -1,5 +1,6 @@
 package edu.icet.ecom.dto;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -7,6 +8,10 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -27,4 +32,7 @@ public class Employee {
     @NotBlank(message = "Department is required")
     @Pattern(regexp = "HR|IT|Finance|Operations", message = "Department must be HR, IT, Finance, or Operations")
     private String department;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
